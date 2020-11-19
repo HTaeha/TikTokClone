d48bed0279dc8d5feb196fbedd4cff65258ed884
HTaeha <xogk39@gmail.com>
2020-11-19 16:59:18 +0900

    User information seperated

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -14,7 +14,9 @@ import AntDesign from 'react-native-vector-icons/AntDesign'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import Fontisto from 'react-native-vector-icons/Fontisto'
 
-const Post = () => {
+const Post = (props) => {
+    const { post } = props
+
     const [paused, setPaused] = useState(false)
 
     const onPlayPausePress = () => {
@@ -28,7 +30,7 @@ const Post = () => {
             >
                 <View>
                     <Video
-                        source={{ uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' }}
+                        source={{ uri: post.videoUri }}
                         style={styles.video}
                         onError={(e) => console.log(e)}
                         resizeMode={'cover'}
@@ -39,38 +41,38 @@ const Post = () => {
                     <View style={styles.uiContainer}>
                         <View style={styles.rightContainer}>
                             <View style={styles.profilePictureContainer}>
-                                <Image style={styles.profilePicture} source={{ uri: 'https://homepages.cae.wisc.edu/~ece533/images/cat.png' }} />
-
+                                <Image style={styles.profilePicture} 
+                                source={{ uri: post.user.imageUri }} />
                             </View>
                             <View style={styles.iconContainer}>
                                 <AntDesign name={"heart"} size={40} color="white" />
-                                <Text style={styles.statsLabel}>123</Text>
+                                <Text style={styles.statsLabel}>{post.likes}</Text>
                             </View>
 
                             <View style={styles.iconContainer}>
                                 <FontAwesome name={"commenting"} size={40} color="white" />
-                                <Text style={styles.statsLabel}>123</Text>
+                                <Text style={styles.statsLabel}>{post.comments}</Text>
                             </View>
 
                             <View style={styles.iconContainer}>
                                 <Fontisto name={"share-a"} size={35} color="white" />
-                                <Text style={styles.statsLabel}>123</Text>
+                                <Text style={styles.statsLabel}>{post.shares}</Text>
                             </View>
                         </View>
 
                         <View style={styles.bottomContainer}>
                             <View>
-                                <Text style={styles.handle}>@Rabbit</Text>
-                                <Text style={styles.description}>hahahaha hello</Text>
+                                <Text style={styles.handle}>{post.user.username}</Text>
+                                <Text style={styles.description}>{post.description}</Text>
 
                                 <View style={styles.songRow}>
                                     <Entypo name={"beamed-note"} size={24} color="white" />
-                                    <Text style={styles.songName}>Nf - The search</Text>
+                                    <Text style={styles.songName}>{post.songName}</Text>
                                 </View>
                             </View>
                             <Image
                                 style={styles.songImage}
-                                source={{ uri: 'https://homepages.cae.wisc.edu/~ece533/images/cat.png' }}
+                                source={{ uri: post.songImage }}
                             />
                         </View>
                     </View>
